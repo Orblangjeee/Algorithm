d24d34711d374852158038b3a0919b8c63b6f135
Orblangjeee <136034429+Orblangjeee@users.noreply.github.com>
2024-02-24 17:42:10 +0900

    [Bronze V] Title: 아스키 코드, Time: 72 ms, Memory: 5472 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.cs
@@ -0,0 +1,12 @@
+
+namespace algorithm
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+         {
+            int index = char.Parse(Console.ReadLine());
+            Console.WriteLine(index);
+        }
+    }
+}
