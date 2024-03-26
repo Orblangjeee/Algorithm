7b8552a06febee8109de975e53c77b4b653c9ee7
Orblangjeee <136034429+Orblangjeee@users.noreply.github.com>
2024-03-26 17:05:14 +0900

    [Bronze I] Title: 평균, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/1546. 평균/평균.cc
+++ 백준/Bronze/1546. 평균/평균.cc
@@ -4,14 +4,12 @@ using namespace std;
 int main() {
 	int n = 0, input = 0, highScore = 0, sum =0;
 	cin >> n;
-	int score[1000];
 	for (int i = 0; i < n; i++) {
 		cin >> input;
 		sum += input;
 		if (highScore < input) {
 			highScore = input;
 		}
-		score[i] = input;
 	}
 
 	cout << sum / (double)highScore * 100 / n;
