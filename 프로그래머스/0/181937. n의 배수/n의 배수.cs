57106451ee1ab51864a025b11d711a12acb43be8
Orblangjeee <136034429+Orblangjeee@users.noreply.github.com>
2025-08-19 09:24:34 +0900

    [level 0] Title: n의 배수, Time: 0.26 ms, Memory: 31.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181937. n의 배수/n의 배수.cs
@@ -0,0 +1,24 @@
+using System;
+
+public class Solution
+{
+    int answer;
+    public int solution(int num, int n)
+    {
+        if (num >= 2 && num <= 100 && n >= 2 && n <= 9)
+        {
+           if (num % n == 0)
+            {
+                answer = 1;
+               
+            }
+            else
+            {
+                answer = 0;
+            }
+            
+        } 
+       return answer;
+       
+    }
+}
