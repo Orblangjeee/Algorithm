9c67402518d611e2b2d07901210ac2adad441f8c
Orblangjeee <136034429+Orblangjeee@users.noreply.github.com>
2026-04-20 14:59:58 +0900

    [Bronze I] Title: 달팽이는 올라가고 싶다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int A, B, V, day = 1;
+	cin >> A >> B >> V;
+	
+	day += (V - A) / (A - B);
+	if ((V - A) % (A - B) > 0)
+		day += 1;
+
+	cout << day;
+}
+
